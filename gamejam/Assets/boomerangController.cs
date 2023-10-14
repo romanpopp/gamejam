cd062f3d15b8b5b23f82573eb87a551284b36206
romanpopp <romanpopp00@gmail.com>
2023-10-13 23:26:02 -0500

    IT WORKS!!! BOOMERANGS!!!!!!!

--- gamejam/Assets/boomerangController.cs
+++ gamejam/Assets/boomerangController.cs
@@ -6,30 +6,25 @@ using UnityEngine;
 public class boomerangController : MonoBehaviour
 {
     private GameObject player;
-    private float speed = 5;
+    public float speed;
+    public float speedDecay;
+    public float rotationRate;
 
     private Vector2 movementDirection;
     
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (speed > 0)
-        {
-            movementDirection = new(
-            player.transform.position.x + transform.position.x,
-            player.transform.position.y + transform.position.y);
-            transform.Translate(speed * Time.deltaTime * movementDirection, Space.World);
-            speed -= 5 * Time.deltaTime;
-        }
-        else
-        {
-            movementDirection = new(
-            player.transform.position.x - transform.position.x,
-            player.transform.position.y - transform.position.y);
-            transform.Translate(-1 * speed * Time.deltaTime * movementDirection, Space.World);
-            speed -= 5 * Time.deltaTime;
-        }
-        
+        speed -= speedDecay;
+
+        movementDirection = new(
+        transform.position.x - player.transform.position.x,
+        transform.position.y - player.transform.position.y);
+        movementDirection.Normalize();
+        transform.Translate(speed * Time.deltaTime * movementDirection, Space.World);
+
+        Vector3 rotation = new(0, 0, rotationRate);
+        transform.Rotate(rotation);
     }
 
     public void SetPlayer(GameObject player)
